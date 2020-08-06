7267dd5cf5f13795e7afde3afd095cb114fb7384
shadoweb <shadoweb@qq.com>
2020-08-06 08:54:03 +0800

    1.2

--- inc/inc.php
+++ inc/inc.php
@@ -1,5 +1,11 @@
 <?php
-
+function getNeedBetween($kw,$mark1,$mark2){
+$st =stripos($kw,$mark1);
+$ed =stripos($kw,$mark2);
+if(($st==false||$ed==false)||$st>=$ed) return substr($kw,0,stripos($kw,$mark2));
+$kw=substr($kw,($st+1+2),($ed-$st-1-2));
+return $kw;
+}
 
 function getInfo($date){
 header("Content-Type: text/html; charset=utf-8");
@@ -126,25 +132,47 @@ function getDayImg($idx){
 //bing数据次序从-1开始,idx最多获取到前16天.idx=-1&n=8 和 idx=7&n=8 分两次可获取全部
 if($idx<7) $idx = -1;
 else $idx=7;
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,'http://www.bing.com/HPImageArchive.aspx?format=js&cc=cn&pid=hp&og=1&idx='.$idx.'&n=8');
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent: Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36'));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_ENCODING, "gzip");
-$output = curl_exec($ch);
-curl_close($ch);
-$content = json_decode($output);
-$content_arr = objtoarr($content);
-  for($i=0;$i<7;$i++){
-    $tarry = $content_arr['images'][$i];
-    $name = $tarry['enddate'];
-    $tarry['info'] = getInfo($name) ;
-    $tjson = json_encode($tarry);
-    if(!file_exists('json/'.$name.'.json')){
-      savejson('json/'.$name.'.json',$tjson);
-    }
-  }
+$url = 'http://www.bing.com/HPImageArchive.aspx?format=js&cc=cn&pid=hp&og=1&idx='.$idx.'&n=8';
+     $curl = curl_init(); // 启动一个CURL会话
+      //以下三行代码解决https图片访问受限问题
+     $dir = pathinfo($url);//以数组的形式返回图片路径的信息
+     $host = $dir['dirname'];//图片路径
+     $ref = $host.'/';
+      if(strstr($ref, 'bing'))  $ref = 'http://cn.bing.com/';
+      curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址    
+      curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // 对认证证书来源的检查
+      curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); // 从证书中检查SSL加密算法是否存在
+      curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
+      curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
+      if($ref){
+        curl_setopt($curl, CURLOPT_REFERER, $ref);//带来的Referer
+      }else{
+        curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
+      }
+      curl_setopt($curl, CURLOPT_HTTPGET, 1); // 发送一个常规的Post请求
+      curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
+      curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
+      $output = curl_exec($curl);
+      if (curl_errno($curl)) {
+        echo 'Errno'.curl_error($curl);
+      }
+      curl_close($curl);
+      if($output === FALSE ){
+        return false;
+      }else{
+      $content = json_decode($output);
+      $content_arr = objtoarr($content);
+        for($i=0;$i<7;$i++){
+          $tarry = $content_arr['images'][$i];
+          $name = $tarry['enddate'];
+          $tarry['info'] = getInfo($name) ;
+          $tjson = json_encode($tarry);
+          if(!file_exists('json/'.$name.'.json')){
+            savejson('json/'.$name.'.json',$tjson);
+          }
+        }
+      }
 }
 
 function objtoarr($obj){
--- index.php
+++ index.php
@@ -3,8 +3,9 @@
     <title>Bing壁纸下载</title>
   </head>
   <body>
-<?php 
+<?php
 header("Content-type: text/html; charset=utf-8");
+error_reporting(0); 
 require('inc/inc.php');
 $url_pre = 'https://cn.bing.com';
     getDayImg('-1');//保存最近1-7天的json数据
@@ -16,23 +17,24 @@ $url_pre = 'https://cn.bing.com';
     $next = $page + 1;
     echo "<div style='width:100%;margin:0 auto;text-align:center;'><p><a href='/?p=".$pre."'>前一页</a>&nbsp;&nbsp;<a href='/?p=".$next."'>后一页</a></p>";
     ?>
-<ul style="list-style:none;width:1200px;margin:0 auto;padding:0;">
+<ul style="list-style:none;max-width:1200px;margin:0 auto;padding:0;">
 <?php
     $torder = ($page-1)*$pageSize;//每页第一个排序.
     $tdate = date('Ymd',strtotime($now.'-'.$torder.' day'));//每页第一个日期.
     for($i=0;$i<$pageSize;$i++){
       $t = $i - 1;
       $name = date('Ymd',strtotime($tdate.'-'.$t.' day'));
-      if(!file_exists('json/'.$tdate.'.json')) exit('<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><script>alert("没有数据啦！");window.history.back(-1);</script>');//如果第一个不存在.
+      if(!file_exists('json/'.$tdate.'.json')) {getDayImg('-1');exit('<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><script>alert("没有数据呀！请刷新后再试.");window.history.back(-1);</script>');}//如果第一个不存在.
       if(file_exists('json/'.$name.'.json')){
           $json_file = fopen('json/'.$name.'.json','r');
           $tfile = json_decode(fgets($json_file), true);
+          fclose($json_file);
           $timg = $url_pre.$tfile['url'];
           $turl = $tfile['info']['url'];
-          $tadd = count($turl)!=0 ? "[".$turl['cy'].",".$turl['ct'].']':'';
+          $tadd = getNeedBetween($tfile['copyright'],'，','(');//count($turl)!=0 ? "[".$turl['cy'].",".$turl['ct'].']':'';
           if (!file_exists('images/simg/'.$name.'.jpg')) $nimg = save($timg,$name);//如果图片不存在,则下载图片
           else $nimg = 'images/simg/'.$name.'.jpg';
-          $con = "<li style='display: inline-block;width:96%;margin: 2%;max-width:320px;overflow: hidden;border: 1px solid #e3e3e3;padding: 5px;'><a href='img.php?img=".$name."' target='_blank' alt='".$tfile['copyright']."'><img src='".$nimg."' style='width:96%;margin:0 2%;max-width:320px;' ></a><p style='height:20px;'>".date('Y-m-d',strtotime($tfile['enddate']))."</p><p style='height:20px;'>".$tadd."</p></li>";
+          $con = "<li style='display: inline-block;width:96%;margin: 2%;max-width:320px;overflow: hidden;border: 1px solid #e3e3e3;padding: 5px;'><a href='img.php?img=".$name."' target='_blank' alt='".$tfile['copyright']."'><img src='".$nimg."' style='' ></a><p style='height:20px;'>".date('Y-m-d',strtotime($tfile['enddate']))."</p><p style='height:20px;'>".$tadd."</p></li>";
           echo $con;
       }
     }
